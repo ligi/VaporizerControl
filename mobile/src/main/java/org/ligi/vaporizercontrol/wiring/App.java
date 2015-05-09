d753c7e7eb26fc75da6293253f5a92e14ed2ea5e
ligi <ligi@ligi.de>
2015-05-09 15:23:31 +0200

    Introduce interface for Communicator and Crafty as one impl

--- mobile/src/main/java/org/ligi/vaporizercontrol/wiring/App.java
+++ mobile/src/main/java/org/ligi/vaporizercontrol/wiring/App.java
@@ -2,7 +2,7 @@ package org.ligi.vaporizercontrol.wiring;
 
 import android.app.Application;
 import org.ligi.tracedroid.TraceDroid;
-import org.ligi.vaporizercontrol.model.Settings;
+import org.ligi.vaporizercontrol.model.CraftyCommunicator;
 import org.ligi.vaporizercontrol.model.SharedPreferencesSettings;
 import org.ligi.vaporizercontrol.model.VaporizerCommunicator;
 import org.ligi.vaporizercontrol.model.WritableSettings;
@@ -16,7 +16,7 @@ public class App extends Application {
     public void onCreate() {
         TraceDroid.init(this);
         settings = new SharedPreferencesSettings(this);
-        communicator = new VaporizerCommunicator(this);
+        communicator = new CraftyCommunicator(this);
 
         super.onCreate();
     }
