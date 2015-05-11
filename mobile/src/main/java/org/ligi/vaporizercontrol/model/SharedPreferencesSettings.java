a2238aac3a25ae35737ee387f39cfbb7c21881c8
ligi <ligi@ligi.de>
2015-05-11 13:13:08 +0200

    Add missing overrides

--- mobile/src/main/java/org/ligi/vaporizercontrol/model/SharedPreferencesSettings.java
+++ mobile/src/main/java/org/ligi/vaporizercontrol/model/SharedPreferencesSettings.java
@@ -16,30 +16,37 @@ public class SharedPreferencesSettings implements WritableSettings {
         return ctx.getSharedPreferences("settings", Activity.MODE_PRIVATE);
     }
 
+    @Override
     public int getTemperatureFormat() {
         return getPrefs().getInt("temp", TEMPERATURE_CELSIUS);
     }
 
+    @Override
     public void setTemperatureFormat(int format) {
         getPrefs().edit().putInt("temp", format).commit();
     }
 
+    @Override
     public String getAutoConnectMAC() {
         return getPrefs().getString("addr", null);
     }
 
+    @Override
     public void setAutoConnectMAC(final String mac) {
         getPrefs().edit().putString("addr", mac).commit();
     }
 
+    @Override
     public boolean isDisplayUnitWanted() {
         return getPrefs().getBoolean("unit", true);
     }
 
+    @Override
     public void shouldDisplayUnit(final boolean should) {
         getPrefs().edit().putBoolean("unit", should).commit();
     }
 
+    @Override
     public boolean isPreciseWanted() {
         return getPrefs().getBoolean("precise", false);
     }
@@ -49,10 +56,12 @@ public class SharedPreferencesSettings implements WritableSettings {
         return getPrefs().getBoolean("polling", false);
     }
 
+    @Override
     public void shouldPoll(final boolean should) {
         getPrefs().edit().putBoolean("polling", should).commit();
     }
 
+    @Override
     public void shouldBePrecise(final boolean should) {
         getPrefs().edit().putBoolean("precise", should).commit();
     }
