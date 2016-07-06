386f59b5776dc2dafe79d17060a150770f05c9b2
ligi <ligi@ligi.de>
2016-07-06 21:11:45 +0200

    Introduce Day/Night mode and improve settings

--- mobile/src/main/java/org/ligi/vaporizercontrol/ui/SettingsActivity.kt
+++ mobile/src/main/java/org/ligi/vaporizercontrol/ui/SettingsActivity.kt
@@ -2,6 +2,7 @@ package org.ligi.vaporizercontrol.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -9,47 +10,22 @@ import org.jetbrains.anko.*
 import org.ligi.vaporizercontrol.model.Settings
 import org.ligi.vaporizercontrol.wiring.App
 
-public class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
 
-    fun getApp(): App {
-        return applicationContext as App;
-    }
+    val settings by lazy { (applicationContext as App).settings }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val settings = getApp().settings
         val mac = settings.autoConnectMAC
 
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         verticalLayout {
             radioGroup() {
-                radioButton() {
-                    text = "Celsius"
-                    onClick {
-                        settings.temperatureFormat = Settings.TEMPERATURE_CELSIUS
-                    }
-                    id = Settings.TEMPERATURE_CELSIUS;
-                }
-
-                radioButton() {
-                    text = "Fahrenheit"
-                    onClick {
-                        settings.temperatureFormat = Settings.TEMPERATURE_FAHRENHEIT
-                    }
-                    id = Settings.TEMPERATURE_FAHRENHEIT;
-                }
-
-                radioButton() {
-                    text = "Kelvin"
-                    onClick {
-                        settings.temperatureFormat = Settings.TEMPERATURE_KELVIN
-                    }
-                    id = Settings.TEMPERATURE_KELVIN;
-                }
-
-
+                tempFormatRadio(Settings.TEMPERATURE_CELSIUS, "Celsius")
+                tempFormatRadio(Settings.TEMPERATURE_FAHRENHEIT, "Fahrenheit")
+                tempFormatRadio(Settings.TEMPERATURE_KELVIN, "Kelvin")
             }.check(settings.temperatureFormat)
 
             if (settings.autoConnectMAC != null) {
@@ -62,32 +38,57 @@ public class SettingsActivity : AppCompatActivity() {
                 }
             }
 
-            checkBox("display unit") {
-                isChecked = settings.isDisplayUnitWanted
-                onCheckedChange { compoundButton, b ->
-                    settings.shouldDisplayUnit(b);
-                }
+            settingsCheckBox(settings.isDisplayUnitWanted, "display unit") { compoundButton, b ->
+                settings.shouldDisplayUnit(b)
             }
 
-            checkBox("show rlly precise") {
-                isChecked = settings.isPreciseWanted
-                onCheckedChange { compoundButton, b ->
-                    settings.shouldBePrecise(b);
-                }
+            settingsCheckBox(settings.isDisplayUnitWanted, "show rlly precise") { compoundButton, b ->
+                settings.shouldBePrecise(b)
             }
 
-            checkBox("poll ( only activate with old firmwares )") {
-                isChecked = settings.isPollingWanted
-                onCheckedChange { compoundButton, b ->
-                    settings.shouldPoll(b);
-                }
+            settingsCheckBox(settings.isDisplayUnitWanted, "poll ( only activate with old firmwares )") { compoundButton, b ->
+                settings.shouldPoll(b)
             }
+
+            radioGroup() {
+                dayNightRadio(AppCompatDelegate.MODE_NIGHT_YES, "Night")
+                dayNightRadio(AppCompatDelegate.MODE_NIGHT_NO, "Day")
+                dayNightRadio(AppCompatDelegate.MODE_NIGHT_AUTO, "Auto")
+            }.check(settings.nightMode)
         }
 
     }
 
+    private fun _LinearLayout.settingsCheckBox(state: Boolean, label: String, function: (Any?, Boolean) -> Unit) {
+        checkBox(label) {
+            isChecked = state
+            onCheckedChange(function)
+        }
+    }
+
+    private fun _RadioGroup.dayNightRadio(mode: Int, label: String) {
+       radio(mode, label) {
+           settings.nightMode = mode
+           AppCompatDelegate.setDefaultNightMode(mode)
+           recreate()
+       }
+    }
+
+    private fun _RadioGroup.tempFormatRadio(tempFormat: Int, label: String) {
+        radio(tempFormat, label) {
+            settings.temperatureFormat = tempFormat
+        }
+    }
+
+    private fun _RadioGroup.radio(value: Int, label: String, onClick: (View?) -> Unit) {
+        radioButton() {
+            text = label
+            onClick(onClick)
+            id = value
+        }
+    }
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        finish();
+        finish()
         return super.onOptionsItemSelected(item)
     }
 }
