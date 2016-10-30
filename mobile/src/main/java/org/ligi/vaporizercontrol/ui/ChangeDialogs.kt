eba90c52c11fa7de97e9d77f5304ef784acfe295
ligi <ligi@ligi.de>
2016-10-30 20:40:25 +0100

    Leverage Kotlin more

--- mobile/src/main/java/org/ligi/vaporizercontrol/ui/ChangeDialogs.kt
+++ mobile/src/main/java/org/ligi/vaporizercontrol/ui/ChangeDialogs.kt
@@ -2,15 +2,12 @@ package org.ligi.vaporizercontrol.ui
 
 import android.content.Context
 import android.support.v7.app.AlertDialog
-import android.text.Html
 import android.text.method.LinkMovementMethod
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.RadioGroup
-import android.widget.ScrollView
-import android.widget.TextView
+import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.*
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar
+import org.ligi.compat.HtmlCompat
 import org.ligi.vaporizercontrol.model.VaporizerCommunicator
 import org.ligi.vaporizercontrol.util.TemperatureFormatter
 import org.ligi.vaporizercontrol.wiring.App
@@ -22,25 +19,21 @@ object ChangeDialogs {
         discreteSeekBar.max = (2100 - comm.data.setTemperature!!) / 10
         discreteSeekBar.setIndicatorFormatter("%d°")
 
-        if (comm.data.boostTemperature != null) {
-            discreteSeekBar.progress = comm.data.boostTemperature!! / 10
-        }
+        comm.data.boostTemperature?.let { discreteSeekBar.progress = it / 10 }
 
-        discreteSeekBar.setOnProgressChangeListener { discreteSeekBar, i, b -> comm.setBoosterTemperature(i * 10) }
+        discreteSeekBar.setOnProgressChangeListener { discreteSeekBar, progress, b -> comm.setBoosterTemperature(progress * 10) }
 
         AlertDialog.Builder(ctx).setMessage("Set booster temperature").setView(discreteSeekBar).setPositiveButton("OK", null).show()
     }
 
     fun showLEDPercentageDialog(ctx: Context, comm: VaporizerCommunicator) {
-        val discreteSeekBar = DiscreteSeekBar(ctx)
-        discreteSeekBar.max = 100
-        discreteSeekBar.setIndicatorFormatter("%d%%")
-
-        if (comm.data.ledPercentage != null) {
-            discreteSeekBar.progress = comm.data.ledPercentage!!
+        val discreteSeekBar = DiscreteSeekBar(ctx).apply {
+            max = 100
+            setIndicatorFormatter("%d%%")
+            setOnProgressChangeListener { discreteSeekBar, i, b -> comm.setLEDBrightness(i) }
         }
 
-        discreteSeekBar.setOnProgressChangeListener { discreteSeekBar, i, b -> comm.setLEDBrightness(i) }
+        comm.data.ledPercentage?.let { discreteSeekBar.progress = it }
 
         AlertDialog.Builder(ctx).setMessage("Set LED brightness").setView(discreteSeekBar).setPositiveButton("OK", null).show()
     }
@@ -58,7 +51,7 @@ object ChangeDialogs {
             TemperatureSetting(1900, "<a href='http://en.wikipedia.org/wiki/Chamomilla_recutita'>Chamomile</a> Flowers " +
                     "or <a href='http://en.wikipedia.org/wiki/Salvia_officinalis'>Sage</a> Leaves " +
                     "or  <a href='http://en.wikipedia.org/wiki/Thymus_vulgaris'>Thyme</a> Herb")
-            )
+    )
 
 
     fun showTemperatureDialog(ctx: Context, comm: VaporizerCommunicator) {
@@ -67,28 +60,26 @@ object ChangeDialogs {
 
         val scrollView = ScrollView(ctx)
 
-        scrollView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        scrollView.layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
         val lin = LinearLayout(ctx)
         lin.orientation = LinearLayout.VERTICAL
 
-        val discreteSeekBar = DiscreteSeekBar(ctx)
-        discreteSeekBar.min = 40
-        discreteSeekBar.max = 210
-        discreteSeekBar.setIndicatorFormatter("%d°")
-
-        if (comm.data.setTemperature != null) {
-            discreteSeekBar.progress = comm.data.setTemperature!! / 10
+        val discreteSeekBar = DiscreteSeekBar(ctx).apply {
+            min = 40
+            max = 210
+            setIndicatorFormatter("%d°")
         }
 
-        lin.addView(discreteSeekBar)
+        comm.data.setTemperature?.let { discreteSeekBar.progress = it / 10 }
 
+        lin.addView(discreteSeekBar)
 
         for (temperatureSetting in temperatureList) {
             val grp = RadioGroup(ctx)
             grp.orientation = LinearLayout.HORIZONTAL
             grp.setPadding(16, 16, 16, 16)
             val txt = TextView(ctx)
-            txt.text = Html.fromHtml(temperatureSetting.htmlDescription)
+            txt.text = HtmlCompat.fromHtml(temperatureSetting.htmlDescription)
             txt.movementMethod = LinkMovementMethod()
 
             val button = Button(ctx)
