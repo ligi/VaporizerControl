b5a65c76533299ecc28c8357bdac2e9e57e3771b
ligi <ligi@ligi.de>
2016-10-30 20:14:07 +0100

    Write better Kotlin

--- mobile/src/main/java/org/ligi/vaporizercontrol/ui/HelpDialogFragment.kt
+++ mobile/src/main/java/org/ligi/vaporizercontrol/ui/HelpDialogFragment.kt
@@ -13,19 +13,19 @@ import org.ligi.vaporizercontrol.wiring.App
 
 class HelpDialogFragment : DialogFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater!!.inflate(R.layout.help, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
+            = inflater.inflate(R.layout.help, container, false)
+
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val data = (activity.applicationContext as App).vaporizerCommunicator.data
 
-        serial.text = handleNull(data.serial)
-        model.text = handleNull(data.model)
-        hours.text = handleNull(data.hoursOfOperation)
-        version.text = handleNull(data.version)
+        serial.text = data.serial.nullSafeString()
+        model.text = data.model.nullSafeString()
+        hours.text = data.hoursOfOperation.nullSafeString()
+        version.text = data.version.nullSafeString()
 
         help.text = Html.fromHtml(
                 "Find out more about this app and the source-code <a href='http://github.com/ligi/VaporizerControl'>on GitHub</a><br/><br/> Happy vaping!-) ")
@@ -33,8 +33,6 @@ class HelpDialogFragment : DialogFragment() {
 
     }
 
-    fun handleNull(bar: Any?): String {
-        return if (bar == null) "N/A" else bar.toString()
-    }
+    fun Any?.nullSafeString() = if (this == null) "N/A" else this.toString()
 
 }
